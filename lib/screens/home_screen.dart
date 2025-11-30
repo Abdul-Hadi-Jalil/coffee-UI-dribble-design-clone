f4979951e64eaa765558ce4aa62d164711b595c2
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2025-11-30 16:00:31 +0500

    created the home screen search bar, bottom navigation bar and app bar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        backgroundColor: Colors.transparent,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Icon(Icons.person),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // text
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              "Find the best coffee for you",
+              style: GoogleFonts.bebasNeue(fontSize: 56),
+            ),
+          ),
+
+          SizedBox(height: 25),
+
+          // search bar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              cursorColor: Colors.orange,
+              decoration: InputDecoration(
+                hintText: "Find you coffee..",
+                prefixIconColor: Colors.orange,
+                prefixIcon: Icon(Icons.search),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade500),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade500),
+                ),
+              ),
+            ),
+          ),
+
+          //
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ''),
+        ],
+      ),
+    );
+  }
+}
